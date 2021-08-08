8a038c438f6a25244c6788b9d5e4abfbc462731d
Alimzhan Rakhmatulin <rakhmatulin.alim@gmail.com>
2021-08-08 20:13:19 +0200

    dv

--- js/main-script.js
+++ js/main-script.js
@@ -1,12 +1,18 @@
 // JavaScript Document
-$(document).on("scroll", function() {
-    if($(window).scrollTop() > 50) {
-        $(".header").addClass("change-color");
-    } else {
-        //remove the background property so it comes transparent again (defined in your css)
-       $(".header").removeClass("change-color");
-    }
-});
+const header = document.querySelector('header')
+
+
+window.onscroll = function(){
+	var top = window.scrollY;
+	if (top>=50){
+		header.classList.add('active')
+	}else{
+		header.classList.remove('active');
+	}
+} 
+	
+	
+
 
 function myWhatsapp() {
 	window.open('https://wa.me/4917674702020?text=Здравствуйте,%20я%20хотел%20бы%20узнать%20подробнее%20о%20вашей%20продукции.','_blank');}
