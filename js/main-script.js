0ecfea020f7b6a03524557f1e3b4d41f76231778
Alimzhan Rakhmatulin <rakhmatulin.alim@gmail.com>
2021-08-08 19:03:31 +0200

    whatsap

--- js/main-script.js
+++ js/main-script.js
@@ -8,6 +8,5 @@ $(document).on("scroll", function() {
     }
 });
 
-function myFunction() {
-	document.getElementById("demo").innerHTML = "Paragraph changed.";
-}
+function myWhatsapp() {
+	window.open('https://wa.me/4917674702020?text=Здравствуйте,%20я%20хотел%20бы%20узнать%20подробнее%20о%20вашей%20продукции.','_blank');}
